970c813b4f2a0a78ab70655fe2dd97268975b6cc
kbenaicha <154835526+kbenaicha@users.noreply.github.com>
2025-03-11 11:54:42 +0100

    index et footer

--- include/footer.php
+++ include/footer.php
@@ -1,51 +1,56 @@
 <footer class="full-row bg-footer p-0">
     <div class="container">
-        <div  class="row">
+        <div class="row">
             <div class="col-lg-12">
                 <div class="divider py-40">
                     <div class="row">
+                        <!-- Logo et description -->
                         <div class="col-md-12 col-lg-4">
                             <div class="footer-widget mb-4">
-                                <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/logo-white.png" height="60px" alt="image"></a> </div>
-                                <p class="pb-20 text-white">Risus commodo congue augue phasellus morbi hymenaeos ante tincidunt eu orci dictum bibendum lacus platea primis mi lacinia felis gravida natoque bibendum cubilia montes tristique et arcu blandit risus. Lobortis dignissim nam.</p>
-								<p class="pb-20 text-white">Risus commodo congue augue phasellus morbi hymenaeos ante tincidunt eu orci dictum bibendum lacus platea primis mi lacinia felis gravida natoque</p>
+                                <div class="footer-logo mb-4">
+                                    <a href="#"><img class="logo-bottom" src="images/logo/logo-white.png" height="60px" alt="Omnes Immobilier"></a>
                                 </div>
+                                <p class="text-white">
+                                    Omnes Immobilier – Trouvez, Visitez, Concrétisez. L'immobilier simplifié, avec des experts à votre service !
+                                </p>
+                            </div>
                         </div>
+
+                        <!-- Liens rapides -->
                         <div class="col-md-12 col-lg-8">
                             <div class="row">
-                                <div class="col-md-4 col-lg-4">
+                                <div class="col-md-6 col-lg-4">
                                     <div class="footer-widget footer-nav mb-4">
-                                        <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                        <h4 class="widget-title text-white double-down-line-left position-relative">Services</h4>
                                         <ul class="hover-text-primary">
-                                            <li><a href="#" class="text-white">Forum</a></li>
-                                            <li><a href="#" class="text-white">Statistics</a></li>
-                                            <li><a href="#" class="text-white">Terms and Condition</a></li>
-                                            <li><a href="#" class="text-white">Get Support</a></li>
-                                            <li><a href="#" class="text-white">Freequenly Ask Question</a></li>
-                                            <li><a href="#" class="text-white">Contact</a></li>
+                                            <li><a href="#" class="text-white">Nos Agents</a></li>
+                                            <li><a href="property.php" class="text-white">Propriétés</a></li>
+                                            
                                         </ul>
                                     </div>
                                 </div>
-                                <div class="col-md-4 col-lg-4">
+                                <div class="col-md-6 col-lg-4">
                                     <div class="footer-widget footer-nav mb-4">
-                                        <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                        <h4 class="widget-title text-white double-down-line-left position-relative">À propos</h4>
                                         <ul class="hover-text-primary">
-                                            <li><a href="#" class="text-white">À propos</a></li>
-                                            <!--- <li><a href="agents.php" class="text-white">Our Agents</a></li> --->
+                                            <li><a href="about.php" class="text-white">Qui sommes-nous ?</a></li>
+                                            <li><a href="about.php" class="text-white">Notre mission</a></li>
+                                            
                                         </ul>
                                     </div>
                                 </div>
-                                <div class="col-md-4 col-lg-4">
+
+                                <!-- Coordonnées et réseaux sociaux -->
+                                <div class="col-md-12 col-lg-4">
                                     <div class="footer-widget">
-                                        <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                        <h4 class="widget-title text-white double-down-line-left position-relative">Nous contacter</h4>
                                         <ul class="text-white">
-                                            <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>Collins Street West, Victoria 8007, Australia</li>
-                                            <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+1 246-345-0695</li>
-											<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+1 246-345-0695</li>
-                                            <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>helpline@homex.com</li>
+                                            <li><i class="fas fa-map-marker-alt text-white mr-2"></i> 12 Rue de Paris, 75001 Paris</li>
+                                            <li><i class="fas fa-phone-alt text-white mr-2"></i> +33 1 23 45 67 89</li>
+                                            <li><i class="fas fa-envelope text-white mr-2"></i> contact@omnes-immobilier.com</li>
                                         </ul>
                                     </div>
-                                    <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -53,13 +58,17 @@
                 </div>
             </div>
         </div>
+
+        <!-- Copyright -->
         <div class="row copyright">
-            <div class="col-sm-6"> <span class="text-white">© 2025 AJKW</span> </div>
             <div class="col-sm-6">
-                <ul class="line-menu text-white hover-text-primary float-right">
-                    <li><a href="#">Privacy & Policy</a></li>
+                <span class="text-white">© 2025 Omnes Immobilier - Tous droits réservés</span>
+            </div>
+            <div class="col-sm-6 text-right">
+                <ul class="line-menu text-white hover-text-primary">
+                    <li><a href="#">Politique de confidentialité</a></li>
                     <li>|</li>
-                    <li><a href="#"> Site Map</a></li>
+                    <li><a href="#">Plan du site</a></li>
                 </ul>
             </div>
         </div>
